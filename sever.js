fafce5ab494f4c29f3a29aba6e4bf2a69cdefac6
ic0407 <vicky92407@gmail.com>
2024-11-22 12:03:50 +0800

    20241122

--- sever.js
+++ sever.js
@@ -1,18 +1,45 @@
+//install: node js
+//install web server package: express >npm install express
 var express = require("express");
-var sever = express();
+var server = express();
 
-sever.use(express.static(__dirname+"/index_01"));
+//web root
+server.use(express.static(__dirname+"/index_01"));
 
-sever.get("/profolio", (req,res)=>{     
-      //DB        
-      res.send("Profolio");  
+var DB = require("nedb-promises");
+var ProfolioDB = DB.create(__dirname+"/profolio.db");
+var ProfolioDB = DB.create(__dirname+"/services.db");
+
+// ProfolioDB.insert([
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+//     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+//     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+//     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+// ])
+
+server.get("/services", (req, res)=>{
+    //DB find
+    var Services=[
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+    ];
+    res.send(Services);
 });
 
-var DB = require("nedb-promises"); 
-var Profolio = DB.create(__dirname+"/profolio.db");  
-Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+server.get("/profolio", (req,res)=>{
+      //DB
+      ProfolioDB.find({}).then(results=>{
+        if(results != null){
+             res.send(results);
+        }else{
+            res.send("Error!");
+        }
+      })
+     
 
-sever.listen(80,()=>{
-    console.log("Sever is running at port 80.");
 })
-
+server.listen(80, ()=>{
+    console.log("Server is running at port 80.");
+})
